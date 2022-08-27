8b1ff75129ac9b030601bdb386626579eae0a8c8
teddaingoma <teddaingoma@github.com>
2022-08-27 11:22:02 +0100

    Grocery POS system sketch FYP

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/* global bootstrap: false */
+(function () {
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  })()
+
+  /* Collapsible Toogling */
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
